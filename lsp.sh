81459702a4e0636618bf19ed156999ae8339b302
aklk1ng <2628084745@qq.com>
2022-10-12 10:56:12 +0800

    linux update

---
+++ lsp.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo pacman -S clang
+pip install -U jedi-language-server
+suod npm i -g bash-language-servervvv
+npm i -g vscode-langservers-extracted
+go install golang.org/x/tools/gopls@latest
+sudo pacman -S lua-language-server
+sudo npm install -g typescript typescript-language-server
