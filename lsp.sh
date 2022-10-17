c2ab57d3838aa96484e034aec1db5d29bce9cf26
aklk1ng <2628084745@qq.com>
2022-10-17 19:48:41 +0800

    marksman

--- lsp.sh
+++ lsp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-sudo pacman -S clang
-sudo pacman -S python-pip
+sudo pacman -S clang python-pip lua-language-server
 pip install -U jedi-language-server
-suod npm i -g bash-language-servervvv
-npm i -g vscode-langservers-extracted
+suod npm i -g bash-language-servervvv vscode-langservers-extracted
 go install golang.org/x/tools/gopls@latest
-sudo pacman -S lua-language-server
 sudo npm install -g typescript typescript-language-server
+cd /usr/bin/
+sudo wget https://github.com/artempyanykh/marksman/releases/download/2022-09-13/marksman-linux
+sudo mv marksman-linux marksman && sudo chmod +x marksman
