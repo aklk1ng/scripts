11e3a0dfaacc1d6cbb639b7750387413dce9b986
aklk1ng <2628084745@qq.com>
2022-10-26 16:00:56 +0800

    add cmake-language-server

--- lsp.sh
+++ lsp.sh
@@ -3,6 +3,7 @@ sudo pacman -S clang python-pip lua-language-server
 pip install -U jedi-language-server
 suod npm i -g bash-language-servervvv vscode-langservers-extracted
 go install golang.org/x/tools/gopls@latest
+pip install cmake-language-server
 sudo npm install -g typescript typescript-language-server
 cd /usr/bin/
 sudo wget https://github.com/artempyanykh/marksman/releases/download/2022-09-13/marksman-linux
