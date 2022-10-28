a52882bd4a1cf3f8936a383869da2213e5876487
aklk1ng <2628084745@qq.com>
2022-10-28 20:07:11 +0800

    add v2ray

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clash() {
-    ps -ef | grep clash | grep -v grep
+    ps -ef | grep clash
     if [ $? -ne 0 ]
     then
         echo "clash is not running!"
@@ -12,7 +12,7 @@ clash() {
     fi
 }
 pigcha() {
-    ps -ef | grep pigcha | grep -v grep
+    ps -ef | grep pigcha 
     if [ $? -ne 0 ]
     then
         echo "pigcha is not running!"
@@ -23,5 +23,18 @@ pigcha() {
         echo "pigcha is running!"
     fi
 }
+v2ray() {
+    ps -ef | grep v2ray 
+    if [ $? -ne 0 ]
+    then
+        echo "v2ray is not running!"
+    else
+        git config --global http.proxy http://127.0.0.1:18317 && git config --global https.proxy http://127.0.0.1:18317
+        npm config set proxy=http://127.0.0.1:18317
+        export https_proxy=http://127.0.0.1:18317 http_proxy=http://127.0.0.1:18317
+        echo "v2ray is running!"
+    fi
+}
 clash
 pigcha
+v2ray
