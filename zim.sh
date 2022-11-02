bc529a82b6ffa62cd9d7420b0f2069f99a7e7108
aklk1ng <2628084745@qq.com>
2022-11-02 11:21:31 +0800

    update zim.sh

--- zim.sh
+++ zim.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 curl --proxy 127.0.0.1:7890 -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 cp ~/zim/alisa.zsh ~/.zim/alisa.zsh
 cp ~/zim/env.zsh ~/.zim/env.zsh
