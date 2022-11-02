9ab3da21d0c0894076ddd5cef0f41f4022187f03
aklk1ng <2628084745@qq.com>
2022-11-02 11:27:45 +0800

    update zim.sh

--- zim.sh
+++ zim.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
 curl --proxy 127.0.0.1:7890 -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-cp ~/zim/alisa.zsh ~/.zim/alisa.zsh
-cp ~/zim/env.zsh ~/.zim/env.zsh
-cat ~/zim/zimrc >> ~/.zimrc
-cat ~/zim/zshrc >> ~/.zshrc
-source ~/.zshrc
+
+function backup_old_files()
+{
+    old_zim=$HOME"/zim"
+    is_exist=$(is_exist_dir $old_vim)
+    if [ $is_exist == 1 ]; then
+        time=$(get_datetime)
+        backup_zim=$old_zim"_bak_"$time
+        read -p "Find "$old_zim" already exists,backup "$old_zim" to "$backup_zim"? [Y/N] " ch
+        if [[ $ch == "Y" ]] || [[ $ch == "y" ]]; then
+            cp -R $old_zim $backup_zim
+        fi
+    fi
+}
+function copy_cat_files()
+{
+    cp ~/zim/alisa.zsh ~/.zim/alisa.zsh
+    cp ~/zim/env.zsh ~/.zim/env.zsh
+    cat ~/zim/zimrc >> ~/.zimrc
+    cat ~/zim/zshrc >> ~/.zshrc
+    source ~/.zshrc
+}
+
+backup_old_files()
+copy_cat_files()
