1d53a1cbc791c7540f8cbcf547d2525d01c53042
aklk1ng <2628084745@qq.com>
2022-11-08 18:37:14 +0800

    update window-manager-config.sh

--- window-manager-config.sh
+++ window-manager-config.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 yay -S ttf-material-design-icons ttf-joypixels wqy-microhei
-sudo pacman -S clipboard pamixer acpi bat xmodmap dunst rofi feh v2ray i3lock mpd mpc ncmpcpp pactl
+sudo pacman -S xclip pamixer acpi bat xmodmap dunst rofi feh v2ray i3lock mpd mpc ncmpcpp pactl
 pip intall pynvim
