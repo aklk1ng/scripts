4233ea4d3355e660faca4e161a73a66663a871ef
aklk1ng <2628084745@qq.com>
2022-11-08 17:44:36 +0800

    del ohmyzsh.sh and add window-manager-config.sh

--- ohmyzsh.sh
+++
@@ -1,7 +0,0 @@
-#ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp ./config/thm.zsh-theme ~/.oh-my-zsh/themes
---
+++ window-manager-config.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+yay -S ttf-material-design-icons ttf-joypixels wqy-microhei
+sudo pacman -S pamixer acpi bat xmodmap dunst rofi feh v2ray i3lock mpd mpc ncmpcpp pactl
