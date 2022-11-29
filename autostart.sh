02ddc3f76bb41f7ddb6dfaae851b3cc2594d4609
aklk1ng <2628084745@qq.com>
2022-11-29 18:33:14 +0800

    change the wallpaper

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 nm-applet &
 xrandr --dpi 192
 mpd ~/.config/mpd/mpd.conf &
-feh --bg-scale ~/wallpaper/ic8ubjf.png &
+feh --bg-scale ~/wallpaper/wallhaven-wey3zp.png &
 fcitx5 &
 settings() {
     [ $1 ] && sleep $1
