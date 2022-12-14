08a4da8413b994f1a92bf17d856795ffa98f209e
aklk1ng <2628084745@qq.com>
2022-12-14 20:42:55 +0800

    update the lsp.sh

--- lsp.sh
+++ lsp.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 sudo pacman -S clang python-pip gopls jedi-language-server bash-language-server lua-language-server vscode-json-languageserver vscode-html-languageserver vscode-css-languageserver
-pip install -U jedi-language-server
-sudo npm i -g bash-language-server vscode-langservers-extracted cssmodules-language-server
+# pip install -U jedi-language-server
+sudo npm i -g vscode-langservers-extracted cssmodules-language-server
 # go install golang.org/x/tools/gopls@latest
 pip install cmake-language-server
 sudo npm install -g typescript typescript-language-server
-yarn global add yaml-language-server
 cd /usr/bin/
 sudo wget https://github.com/artempyanykh/marksman/releases/download/2022-09-13/marksman-linux
 sudo mv marksman-linux marksman && sudo chmod +x marksman
