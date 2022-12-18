4e469f4f6b26a72dcf8cf5ebfe7dd159d5bc91a7
aklk1ng <2628084745@qq.com>
2022-12-18 15:53:52 +0800

    update the rust-analyzer

--- lsp.sh
+++ lsp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo pacman -S clang python-pip gopls jedi-language-server bash-language-server lua-language-server vscode-json-languageserver vscode-html-languageserver vscode-css-languageserver
+sudo pacman -S clang rust-analyzer python-pip gopls jedi-language-server bash-language-server lua-language-server vscode-json-languageserver vscode-html-languageserver vscode-css-languageserver
 # pip install -U jedi-language-server
 sudo npm i -g vscode-langservers-extracted cssmodules-language-server
 # go install golang.org/x/tools/gopls@latest
