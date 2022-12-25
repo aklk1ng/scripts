207d2d78d2d15c8d0199ac1c62c49c9d57e7ce63
aklk1ng <2628084745@qq.com>
2022-12-25 10:40:27 +0800

    update

--- window-manager-config.sh
+++ window-manager-config.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 yay -S ttf-material-design-icons ttf-joypixels wqy-microhei
-sudo pacman -S xclip pamixer acpi bat xmodmap dunst rofi feh v2ray i3lock mpd mpc ncmpcpp pactl
-pip intall pynvim
+sudo pacman -S xclip pamixer acpi xmodmap dunst rofi feh v2ray i3lock mpd mpc ncmpcpp pactl python nodejs yarn fzf ripgrep
+pip install pynvim
+npm install -g neovim
