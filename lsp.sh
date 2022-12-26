ea40648fdc79ec9af4a8e8c8a9b63555e6e02753
aklk1ng <2628084745@qq.com>
2022-12-26 11:02:40 +0800

    update the marksman

--- lsp.sh
+++ lsp.sh
@@ -5,6 +5,4 @@ sudo npm i -g vscode-langservers-extracted cssmodules-language-server
 # go install golang.org/x/tools/gopls@latest
 pip install cmake-language-server
 sudo npm install -g typescript typescript-language-server
-cd /usr/bin/
-sudo wget https://github.com/artempyanykh/marksman/releases/download/2022-09-13/marksman-linux
-sudo mv marksman-linux marksman && sudo chmod +x marksman
+yay -S marksman-bin
