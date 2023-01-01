b8620edebb26f9851986776de7cc462505bdd33c
aklk1ng <2628084745@qq.com>
2023-01-01 09:56:21 +0800

    update the lsp.sh

--- lsp_debugger.sh
+++ lsp_debugger.sh
@@ -30,10 +30,11 @@ pigcha() {
 clash
 pigcha
 
-sudo pacman -S clang rust-analyzer python-pip gopls jedi-language-server bash-language-server lua-language-server vscode-json-languageserver vscode-html-languageserver vscode-css-languageserver
+sudo pacman -S clang delve rust-analyzer python-pip gopls jedi-language-server bash-language-server lua-language-server vscode-json-languageserver vscode-html-languageserver vscode-css-languageserver
 # pip install -U jedi-language-server
 sudo npm i -g vscode-langservers-extracted cssmodules-language-server
 # go install golang.org/x/tools/gopls@latest
-pip install cmake-language-server
+pip install cmake-language-server debugpy
 sudo npm install -g typescript typescript-language-server
 proxychains yay -S marksman-bin
+brew install llvm
