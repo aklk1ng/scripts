4ce580e210a6042ae540764f51f71c92af4979c8
aklk1ng <2628084745@qq.com>
2023-01-27 09:48:46 +0800

    update `start-clash.sh`

--- start-clash.sh
+++ start-clash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# killall -q clash-linux-amd64-v1.10.0
 # killall -q clash-linux-amd64-v1.12.0
 killall -q clash-linux-386-v1.12.0
 cd /home/cjh/clash
-# ./clash-linux-amd64-v1.10.0 -f glados.yaml -d .
-./clash-linux-amd64-v1.10.0 -f Wallless.yaml -d .
+./clash-linux-386-v1.12.0 -f Wallless.yaml -d .
+# ./clash-linux-amd64-v1.12.0 -f Wallless.yaml -d .
