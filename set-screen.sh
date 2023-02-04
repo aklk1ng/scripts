9c979ff7addb009d8cbabc1b57e0931272a765fc
aklk1ng <2628084745@qq.com>
2023-02-04 15:31:12 +0800

    update rofi and set-screen

--- set-screen.sh
+++ set-screen.sh
@@ -7,44 +7,36 @@
   toggle_auto: 开启/关闭自动分辨率调整开关
 !
 
-INNER_SCREEN=eDP-1
-OUT1_SCREEN=DP-1
-OUt2_SCREEN=DP-2
-CONNECT_SCREEN=$(xrandr | grep DP | grep -v eDP | grep ' connected ' | awk '{print $1}')
-DISCONNECT_SCREEN=$(xrandr | grep DP | grep -v eDP | grep ' disconnected ' | awk '{print $1}')
+INNER_PORT=eDP-1
+OUTPORT1=DP-1
+OUTPORT2=DP-2
 
-_post() {
-    ~/scripts/edit-profile.sh SCREEN_MODE $1
-    ~/scripts/set-wallpaper.sh
-}
 two() {
-    [ ! "$CONNECT_SCREEN" ] && one && return;
-    xrandr --output $INNER_SCREEN   --mode 3200x2000  --pos 1920x325 --scale 1x1 \
-           --output $CONNECT_SCREEN --mode 1920x1080 --pos 0x0      --scale 1x1 --primary \
-           --output $DISCONNECT_SCREEN --off
-    _post TWO
+    # 查找已连接、未连接的外接接口
+    OUTPORT_CONNECTED=$(xrandr | grep -v $INNER_PORT | grep -w 'connected' | awk '{print $1}')
+    OUTPORT_DISCONNECTED=$(xrandr | grep -v $INNER_PORT | grep -w 'disconnected' | awk '{print $1}')
+    [ ! "$OUTPORT_CONNECTED" ] && one && return # 如果没有外接屏幕则直接调用one函数
+    xrandr --output $INNER_PORT --mode 1440x900 --pos 1920x320 --scale 1x1 \
+           --output $OUTPORT_CONNECTED --mode 1920x1080 --pos 0x0 --scale 1x1 --primary \
+           --output $OUTPORT_DISCONNECTED --off
+    feh --randomize --bg-fill ~/Pictures/wallpaper/*.png
 }
 one() {
-    xrandr --output $INNER_SCREEN --mode 3200x2000--pos 0x0 --scale 1x1  --primary  \
-           --output $OUT1_SCREEN --off \
-           --output $OUt2_SCREEN --off
-    _post ONE
+    xrandr --output $INNER_PORT --mode 1440x900 --pos 0x0 --scale 1x1 --primary \
+           --output $OUTPORT1 --off \
+           --output $OUTPORT2 --off
+    feh --randomize --bg-fill ~/Pictures/wallpaper/*.png
 }
 check() {
-    source ~/.profile
-    [ "$SCREEN_CHECK" = "off" ] && return;
-    [ "$CONNECT_SCREEN" ] && [ "$SCREEN_MODE" == "ONE" ] && two;
-    [ ! "$CONNECT_SCREEN" ] && [ "$SCREEN_MODE" != "ONE" ] && two;
-}
-toggle_auto() {
-    source ~/.profile
-    [ "$SCREEN_CHECK" = "off" ] && ~/scripts/edit-profile.sh SCREEN_CHECK on || ~/scripts/edit-profile.sh SCREEN_CHECK off
+    CONNECTED_PORTS=$(xrandr | grep -w 'connected' | awk '{print $1}' | wc -l)
+    CONNECTED_MONITORS=$(xrandr --listmonitors | sed 1d | awk '{print $4}' | wc -l)
+    [ $CONNECTED_PORTS -gt $CONNECTED_MONITORS ] && two # 如果当前连接接口多于当前输出屏幕 则调用two
+    [ $CONNECTED_PORTS -lt $CONNECTED_MONITORS ] && one # 如果当前连接接口少于当前输出屏幕 则调用one
 }
 
 case $1 in
     one) one ;;
     two) two ;;
     check) check ;;
-    toggle_auto) toggle_auto ;;
-    *) two ;;
+    *) check ;;
 esac
