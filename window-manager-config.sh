f352ae8fe269602bb1cdd5dee24c48844c816d6f
aklk1ng <2628084745@qq.com>
2023-03-29 19:31:04 +0800

    update

--- window-manager-config.sh
+++ window-manager-config.sh
@@ -1,36 +1,35 @@
 #! /bin/bash
 clash() {
-  ps -ef | grep clash | grep -v grep
-  if [ $? -ne 0 ]
-  then
-    echo "clash is not running!"
-  else
-    proxy=$(cat ~/.npmrc)
-    if [[ $proxy ]]; then
-      git config --global http.proxy http://127.0.0.1:7890 && git config --global https.proxy http://127.0.0.1:7890
-      npm config set proxy=http://127.0.0.1:7890
-      export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890
+    ps -ef | grep clash | grep -v grep
+    if [ $? -ne 0 ]; then
+        echo "clash is not running!"
+    else
+        proxy=$(cat ~/.npmrc)
+        if [[ $proxy ]]; then
+            git config --global http.proxy http://127.0.0.1:7890 && git config --global https.proxy http://127.0.0.1:7890
+            npm config set proxy=http://127.0.0.1:7890
+            export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890
+        fi
     fi
-  fi
 }
 pigcha() {
-  ps -ef | grep pigcha | grep -v grep
-  if [ $? -ne 0 ]
-  then
-    echo "pigcha is not running!"
-  else
-    proxy=$(cat ~/.npmrc)
-    if [[ $proxy ]]; then
-      git config --global http.proxy http://127.0.0.1:15777 && git config --global https.proxy http://127.0.0.1:15777
-      npm config set proxy=http://127.0.0.1:15777
-      export https_proxy=http://127.0.0.1:15777 http_proxy=http://127.0.0.1:15777
+    ps -ef | grep pigcha | grep -v grep
+    if [ $? -ne 0 ]; then
+        echo "pigcha is not running!"
+    else
+        proxy=$(cat ~/.npmrc)
+        if [[ $proxy ]]; then
+            git config --global http.proxy http://127.0.0.1:15777 && git config --global https.proxy http://127.0.0.1:15777
+            npm config set proxy=http://127.0.0.1:15777
+            export https_proxy=http://127.0.0.1:15777 http_proxy=http://127.0.0.1:15777
+        fi
     fi
-  fi
 }
 clash
 pigcha
 
-proxychains yay -S i3lock-color keyd ttf-material-design-icons  wqy-microhei cava
+proxychains yay -S i3lock-color keyd ttf-material-design-icons wqy-microhei cava
 sudo pacman -S pavucontrol xfce4-power-manager imagemagick network-manager-applet xorg-xsetroot xclip tmux pcmanfm flameshot pamixer acpi light xorg-xbacklight xmodmap dunst libnotify rofi feh v2ray i3lock mpd mpc ncmpcpp pactl python nodejs yarn fzf ripgrep
 pip install pynvim
+brew install stow
 npm install -g neovim
