51444c32511bcb0203259439411daa2c30f9944d
aklk1ng <2628084745@qq.com>
2023-05-01 19:19:03 +0800

    update

--- proxy.sh
+++ proxy.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 clash() {
     ps -ef | grep clash | grep -v grep
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ]; then
         echo "clash is not running!"
     else
         git config --global http.proxy http://127.0.0.1:7890 && git config --global https.proxy http://127.0.0.1:7890
@@ -10,16 +9,5 @@ clash() {
         export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890
     fi
 }
-pigcha() {
-    ps -ef | grep pigcha | grep -v grep
-    if [ $? -ne 0 ]
-    then
-        echo "pigcha is not running!"
-    else
-        git config --global http.proxy http://127.0.0.1:15777 && git config --global https.proxy http://127.0.0.1:15777
-        npm config set proxy=http://127.0.0.1:15777
-        export https_proxy=http://127.0.0.1:15777 http_proxy=http://127.0.0.1:15777
-    fi
-}
+
 clash
-pigcha
