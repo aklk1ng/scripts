7d1b37a2c289fe6072362376b1569e5652ee1676
aklk1ng <2628084745@qq.com>
2024-09-28 17:30:37 +0800

    update

--- start-clash.sh
+++ start-clash.sh
@@ -1,62 +1,27 @@
 #!/bin/bash
 
-si=0
-set_menu() {
-    menu_items[$si]=$1
-    cmds[$si]=$2
-    si=$(($si + 1))
-}
-
-get_config_files() {
-    for file in $(ls *.yaml); do
-        file_name=${file%.*}
-        set_menu $file_name "clash -f $file -d . &"
-    done
-}
-
-_list() {
-    for ((i = 0; i < ${#menu_items[@]}; i++)); do
-        _item=${menu_items[$i]}
-        echo "${_item[@]}"
-    done
-}
-
-_index() {
-    for ((i = 0; i < ${#menu_items[@]}; i++)); do
-        _item=${menu_items[$i]}
-        [[ "$_item" == "$1" ]] && echo $i && break
-    done
-}
-
 prev_hook() {
-    killall -q clash*
+    killall -q clash
     cd $HOME/clash || return
 }
 
-_preview() {
-    index=$(_index "$1")
-    echo ${cmds[index]}
-}
-
-_clash() {
+main() {
     prev_hook
-    get_config_files
-    proxy=$(
-        _list | fzf \
+    config=$(
+        ls -a *.yaml | rev | cut -d. -f2- | rev | fzf \
             --cycle \
-            --header='switch clash proxy' \
+            --header='Switch clash config' \
             --header-first \
             --height=10 \
             --preview '' \
             --preview-window=bottom:1:wrap,border-up
     )
-    [[ -z "$proxy" ]] && exit 0
-    [[ -z "$(_index "$proxy")" ]] && echo $proxy && exit 0
-    exec $(_preview "$proxy")
+    [[ -z "$config" ]] && exit 0
+    exec $(echo clash -f "$config".yaml -d . &)
 }
 
 case "$1" in
 *)
-    _clash
+    main
     ;;
 esac
